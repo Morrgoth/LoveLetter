52399d574dc9a8adec2dca1ee47e4c64322210c5
bemantec <amentbence@gmail.com>
2022-04-27 20:25:24 +0200

    Added a small Client-Server demo with GUI

--- love_letter/src/main/java/bb/love_letter/Server.java
+++ love_letter/src/main/java/bb/love_letter/Server.java
@@ -1,4 +1,29 @@
 package bb.love_letter;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    static final int PORT = 6868;
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+
+            while(true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    OutputStream output = socket.getOutputStream();
+                    PrintWriter writer = new PrintWriter(output, true);
+                    writer.println("Hi there, I am the server! beep boop");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
