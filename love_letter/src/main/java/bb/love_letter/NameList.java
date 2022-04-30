92505f727466a80e3c81c28a898a28c0ceb26abe
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2022-04-30 18:40:47 +0200

    testPush

---
+++ love_letter/src/main/java/bb/love_letter/NameList.java
@@ -0,0 +1,17 @@
+package bb.love_letter;
+import java.util.ArrayList; // import the ArrayList class
+
+
+public class NameList {
+    ArrayList<String> nameList = new ArrayList<String>(); // Create an ArrayList object
+}
+
+adden
+l;schen
+vergleichen
+        auf searaten branch
+
+public void String Name
+if name .equals(String)
+while ()
+    if
