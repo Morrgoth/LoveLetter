1178da1596ff7d35fcbd3225098b2c39c0037deb
bemantec <amentbence@gmail.com>
2022-05-01 12:17:47 +0200

    Added logging of server ip and port

--- love_letter/src/main/java/bb/love_letter/Server.java
+++ love_letter/src/main/java/bb/love_letter/Server.java
@@ -1,20 +1,18 @@
 package bb.love_letter;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
 public class Server {
     static final int PORT = 6868;
 
     public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(PORT);
 
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            System.out.println("The server is running on " + inetAddress.getHostAddress() + ":" + PORT);
             while(true) {
-                try {
-                    Socket socket = serverSocket.accept();
+                try (Socket socket = serverSocket.accept();) {
                     OutputStream output = socket.getOutputStream();
                     PrintWriter writer = new PrintWriter(output, true);
                     writer.println("Hi there, I am the server! beep boop");
