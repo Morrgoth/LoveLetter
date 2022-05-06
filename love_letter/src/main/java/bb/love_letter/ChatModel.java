7a49ae0cbb34f70b1d667225bb5e2a555c066151
Zeynab Baiani <>
2022-05-06 17:45:31 +0200

    added chat model

--- love_letter/src/main/java/bb/love_letter/ChatModel.java
+++ love_letter/src/main/java/bb/love_letter/ChatModel.java
@@ -1,8 +1,25 @@
 package bb.love_letter;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class ChatModel {
-   // private ObservableList chatMessages = new ObservableList;
+   private ObservableList <ChatMessage> chatMessageObservableList = FXCollections.observableArrayList();
+   private ObservableList<User> userObservableList = FXCollections.observableArrayList();
 
+    public ObservableList<ChatMessage> getChatMessageObservableList() {
+        return chatMessageObservableList;
+    }
+
+    public void addChatMessage(ChatMessage chatMessage){
+        chatMessageObservableList.add(chatMessage);
+    }
+
+    public ObservableList<User> getUserObservableList() {
+        return userObservableList;
+    }
+
+    public void addUser(User user){
+        userObservableList.add(user);
+    }
 }
