32b2c54aa1f99e348e841e74bddd2cb559cb0988
engint <Tolga.Engin@campus.lmu.de>
2022-05-07 16:20:43 +0200

    userEvent klasse wurde gefertigt

---
+++ love_letter/src/main/java/bb/love_letter/UserEvent.java
@@ -0,0 +1,36 @@
+package bb.love_letter;
+
+public class UserEvent {
+    //wrapper klasse für User
+
+    //attributes
+    private User user;
+    private UserEventType userEventType;
+
+    //konstruktor
+    public UserEvent(User user, UserEventType userEventType){
+        this.user=user;
+        this.userEventType=userEventType;
+    }
+
+    //getter methode
+    public User getUser(){
+        return user;
+    }
+    public UserEventType getUserEventType(){
+        return userEventType;
+    }
+
+    public static enum UserEventType {
+        LOGIN_REQUEST,
+        LOGOUT_REQUEST,
+        LOGIN_CONFIRMATION,
+        LOGIN_ERROR
+    }
+
+}
+
+
+
+
+
