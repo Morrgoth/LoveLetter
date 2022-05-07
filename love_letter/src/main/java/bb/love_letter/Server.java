d43c7f3589e78f2587fe8fbd73f776fffd86ce9f
Veronika <heckelv@cip.ifi.lmu.de>
2022-05-07 14:47:51 +0200

    Server - new Update

--- love_letter/src/main/java/bb/love_letter/Server.java
+++ love_letter/src/main/java/bb/love_letter/Server.java
@@ -8,6 +8,26 @@ import java.util.Scanner;
 public class Server {
     static final int PORT = 6868;
     private static UserList userList = new UserList();
+    private static ArrayList<Thread> userThreads = new ArrayList<>();
+
+
+    //login -Methode wurde von Veronika Heckel bearbeitet
+    private static void login(Envelope envelope, Socket socket, ObjectOutputStream output, ObjectInputStream input){
+        if (envelope.getType().equals("User")) {
+            User user = (User)envelope.getPayload();
+            if (userList.addName(user)) {
+                System.out.println(user.getName() + " has entered Chatroom!");
+                ServerSessionHandler serverSessionHandler = new ServerSessionHandler(socket, output, input);
+                Thread thread = new Thread(serverSessionHandler);
+                thread.start();
+                userThreads.add(thread);
+            }
+        }else{
+            System.out.println("Error: Unauthorized request!");
+        }
+    }
+
+
 
     public static void main(String[] args) {
 
@@ -16,46 +36,19 @@ public class Server {
             InetAddress inetAddress = InetAddress.getLocalHost();
             System.out.println("The server is running on " + inetAddress.getHostAddress() + ":" + PORT);
 
-            //while -Schleife + Switch - Case - Fälle wurden von Veronika Heckel bearbeitet
+            //while -Schleife wurden von Veronika Heckel bearbeitet
             while (true) {
                 try {
                     Socket socket = serverSocket.accept();
                     ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
                     ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
                     Envelope envelope = (Envelope) input.readObject();
-                    ServerSessionHandler clientSock = new ServerSessionHandler(socket, output, input);
-                    Thread thread = new Thread(clientSock);
-                    thread.start();
-                    switch (envelope.getType()) {
-                        case "User":
-                            boolean login = false;
-                            while (!login) {
-                                Scanner sc = new Scanner(System.in);
-                                User user = new User(sc.nextLine());
-                                if (userList.addName(user)) {
-                                    login = true;
-                                    System.out.println(user.getName() + " has entered Chatroom!");
-                                }
-                            }
-                        case "ChatMessage":
-                            ChatMessage chatMessage = (ChatMessage) envelope.getPayload();
-                            if(chatMessage.getMessage().equals("bye")){
-                               if(userList.removeName(chatMessage.getSender())){
-                                   System.out.println(chatMessage.getSender() + " leaves the Chatroom.");
-                               }
-                            }else{
-                                  System.out.println(chatMessage.getMessage());
-
-
-                            }
-
-                    }
-
-                } catch (ClassNotFoundException e) {
+                    login(envelope, socket, output, input);
+                }catch (ClassNotFoundException e) {
                     throw new RuntimeException(e);
                 }
             }
-        } catch (IOException e) {
+        }catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
