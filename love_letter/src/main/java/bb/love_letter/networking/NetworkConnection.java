0b2cde5d7acbce56ce33aaeb298598cda310cbd1
bemantec <amentbence@gmail.com>
2022-05-10 16:18:25 +0200

    Added javadocs to the NetworkConnection class.

--- love_letter/src/main/java/bb/love_letter/networking/NetworkConnection.java
+++ love_letter/src/main/java/bb/love_letter/networking/NetworkConnection.java
@@ -5,6 +5,13 @@ import bb.love_letter.game.User;
 import java.io.*;
 import java.net.Socket;
 
+/**
+ * This is a Singleton, a central storage location for the Network Connection information required by several classes
+ * on the Client Side. After the initialization it provides access to the Socket, as well as the Input- and OutputStreams
+ * so the Client can send and receive messages.
+ *
+ * @author Tolga Engin
+ */
 public class NetworkConnection {
     private static NetworkConnection instance = null;
 
@@ -14,19 +21,41 @@ public class NetworkConnection {
     DataOutputStream dataOutputStream;
     NetworkConnection(){}
 
+    /**
+     * @return The logged-in User
+     */
     public User getUser(){
         return user;
     }
+
+    /**
+     * @return The Socket of the logged-in User.
+     */
     public Socket getSocket(){
         return socket;
     }
+
+    /**
+     * @return The InputStream of the logged-in User, all messages from the Server are received through it.
+     */
     public DataInputStream getInputStream(){
         return dataInputStream;
     }
+
+    /**
+     * @return The OutputStream of the logged-in User, all messages are sent through it to the Server.
+     */
     public DataOutputStream getOutputStream(){
         return dataOutputStream;
     }
 
+    /**
+     * The initialization method of this Singleton should be called only once, upon the successful login of the User.
+     * @param socket The Socket of the already logged-in User.
+     * @param dataInputStream The InputStream of the already logged-in User
+     * @param dataOutputStream The OutputStream of the already logged-in User.
+     * @param user The User information of the already logged-in User.
+     */
     public void init(Socket socket, DataInputStream dataInputStream, DataOutputStream dataOutputStream, User user) {
         this.socket = socket;
         this.dataInputStream = dataInputStream;
@@ -34,6 +63,9 @@ public class NetworkConnection {
         this.user = user;
     }
 
+    /**
+     * @return The only instance of the NetworkConnection class.
+     */
     public static NetworkConnection getInstance() {
         if (instance == null) {
             instance = new NetworkConnection();
