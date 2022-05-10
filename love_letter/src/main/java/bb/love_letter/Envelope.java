24a9ec75566eb6b70ccb504066c1771eca59d29e
bemantec <amentbence@gmail.com>
2022-05-10 13:54:39 +0200

    Added javadocs to the Envelope class.

--- love_letter/src/main/java/bb/love_letter/Envelope.java
+++ love_letter/src/main/java/bb/love_letter/Envelope.java
@@ -6,21 +6,39 @@ import com.google.gson.JsonObject;
 
 import java.io.*;
 
+/**
+ * Envelope is used for Client-Server communication, by convention whenever we exchange data betweeen Client and Server
+ * it must be wrapped by the sender in an Envelope and turned to JSON, and it must be deserialized from JSON by
+ * the receiver and then unwrapped.
+ *
+ * @author Muqiu Wang
+ */
 public class Envelope implements Serializable {
     public Object payload;
     public TypeEnum type;
 
-    //enum * user & chatMessage ) speichert in string type
+    /**
+     * TypeEnum is used to store the original type of the payload of the Envelope which is necessary to know in order
+     * to unwrap the Envelope and get the payload back.
+     */
     public enum TypeEnum{
         USEREVENT,
         CHATMESSAGE
     }
 
+    /**
+     * This constructor is used if you wish to create a new Envelope.
+     * @param payload The Object to be exchanged between Client and Server.
+     * @param type The original type of the payload.
+     */
     public Envelope(Object payload, TypeEnum type){
         this.payload = payload;
         this.type = type;
     }
 
+    /**
+     * This constructor is only used for the deserialization.
+     */
     public Envelope() {
         this.payload = null;
         this.type = null;
@@ -29,23 +47,34 @@ public class Envelope implements Serializable {
     public Object getPayload(){
         return payload;
     }
-
-    public void setPayload(Object payload) {
-        this.payload = payload;
-    }
-
     public TypeEnum getType(){
         return type;
+    }
+
+    /**
+     * This method is only used for the deserialization.
+     * @param payload The Object to be stored in the Envelope.
+     */
+    public void setPayload(Object payload) {
+        this.payload = payload;
     }
     public void setType(TypeEnum type) {
         this.type = type;
     }
 
+    /**
+     * @return JSON String to be sent through the network.
+     */
     public String toJson() {
         Gson gson = new GsonBuilder().registerTypeAdapter(Envelope.class, new EnvelopeSerializer()).create();
         return gson.toJson(this);
     }
 
+    /**
+     * This static method is used to recover an Envelope from its serialized JSON format.
+     * @param json This is a JSON String received through the network.
+     * @return An Envelope that has the same contents as the one which was originally sent through the Network.
+     */
     public static Envelope deserializeEnvelopeFromJson(String json) {
         Gson gson = new Gson();
         JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
