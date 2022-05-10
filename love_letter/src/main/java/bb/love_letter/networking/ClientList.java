c73a4df502c5e7f6705cbe4d59356cf435d9684a
bemantec <amentbence@gmail.com>
2022-05-10 16:03:54 +0200

    Added javadocs to ClientList

--- love_letter/src/main/java/bb/love_letter/networking/ClientList.java
+++ love_letter/src/main/java/bb/love_letter/networking/ClientList.java
@@ -5,13 +5,24 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Set;
-// Create an ArrayList object
 
-//Diese Klasse wurde von Veronika Heckel bearbeitet
+/**
+ * The ClientList is used by the Server and ServerThread to keep track of the authenticated users, and store their
+ * Socket connections to do server. This class is also responsible for logging out users and making sure their
+ * connections are closed.
+ *
+ * @author Veronika Heckel
+ * @author Philipp Keyzman
+ */
 public class ClientList {
-     public HashMap<User, Socket> clientList = new HashMap<User,Socket>();
+    public HashMap<User, Socket> clientList = new HashMap<User,Socket>();
 
-    // adds a clientName to UserList-array
+    /**
+     * @param user The User to be logged in, the username of this user must be unique in the list
+     * @param socket The connection of the User to the Server
+     * @return Returns true if the user was added successfully, false if some error occured (e.g. the username
+     * is already in use)
+     */
     public boolean addClient(User user, Socket socket) {
         if (!containsClient(user)) {
             clientList.put(user, socket);
@@ -22,13 +33,25 @@ public class ClientList {
         }
     }
 
+    /**
+     * @return Returns a Set of the currently logged-in Users without their Socket connections
+     */
     public Set<User> getUsers() {
         return clientList.keySet();
     }
 
+    /**
+     * @param user The User whose Socket connection is needed
+     * @return The Socket connection of the User
+     */
     public Socket getClientSocket(User user) {
         return clientList.get(user);
     }
+
+    /**
+     * @param user The User to be checked against the list
+     * @return Returns true if a User with the name of the provided user parameter is already in the list, false otherwise
+     */
     public boolean containsClient(User user) {
         for (User u : clientList.keySet()) {
             if (u.equals(user)) {
@@ -38,7 +61,11 @@ public class ClientList {
         return false;
     }
 
-    //removeName a clientName from UserList-array
+    /**
+     * @param user The User to be removed from the list, and whose connection should be closed
+     * @return Returns true if the User was successfully removed, false otherwise
+     * @throws IOException
+     */
     public boolean removeClient(User user) throws IOException {
         if (containsClient(user)) {
             Socket socket = clientList.get(user);
@@ -50,7 +77,9 @@ public class ClientList {
         }
     }
 
-    //print whole userList to client
+    /**
+     * Prints the usernames of the logged-in Users to the command line
+     */
     public void printClientList(){
         for (User u: clientList.keySet() ) {
             System.out.println(u.getName());
