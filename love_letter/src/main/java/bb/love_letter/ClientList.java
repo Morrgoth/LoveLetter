30ace9d16b53311b3b0b657038d60897abf1f90d
Veronika <heckelv@cip.ifi.lmu.de>
2022-05-10 15:19:27 +0200

    Javadocs für die Klasse ClientList

--- love_letter/src/main/java/bb/love_letter/ClientList.java
+++ love_letter/src/main/java/bb/love_letter/ClientList.java
@@ -4,13 +4,26 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
-// Create an ArrayList object
 
-//Diese Klasse wurde von Veronika Heckel bearbeitet
+
+/**
+ * ClientList - Class represents every user with its related socket listet in a hashmap.
+ * contains all Methods for the related actions for the ClientList:
+ * adding/removing a new User and to print out the whole list
+ *
+ * @author Veronika
+ */
 public class ClientList {
      public HashMap<User, Socket> clientList = new HashMap<User,Socket>();
 
-    // adds a clientName to UserList-array
+
+    /**
+     * Method checks if the name of a new incoming User already exists by  calling the Method contains Client
+     * If not the new User and its socket connection  will be added to the ClientList.
+     * @param user is an Object of the User class represented by the name
+     * @param socket is the connection from the Client/ User to the server
+     * @return true if the User could be added to the list
+     */
     public boolean addClient(User user, Socket socket) {
         if (!containsClient(user)) {
             clientList.put(user, socket);
@@ -28,6 +41,13 @@ public class ClientList {
     public Socket getClientSocket(User user) {
         return clientList.get(user);
     }
+
+    /**
+     * checks if the User's name is already in the ClientList.
+     * Method is called in the Methods addName and removeName
+     * @param user is one Object of the Class user represented by the name
+     * @return true if a User's name already exists in the ClientList
+     */
     public boolean containsClient(User user) {
         for (User u : clientList.keySet()) {
             if (u.equals(user)) {
@@ -37,7 +57,15 @@ public class ClientList {
         return false;
     }
 
-    //removeName a clientName from UserList-array
+    /**
+     * Method to remove a Client from the ClientList.
+     * calls the Method containsClient to check if the given name exists in the list and can be removed
+     * if yes the socket for the User that needs to be removed will be closed and the User deleted from the Clientlist
+     * @param user is an Object of the Class User represented by the name
+     * @return true if the socket can be closed properly and the user can be removed successfully from the ClientList
+     * @throws IOException when given an invalid input
+     *
+     */
     public boolean removeClient(User user) throws IOException {
         if (containsClient(user)) {
             Socket socket = clientList.get(user);
@@ -49,7 +77,11 @@ public class ClientList {
         }
     }
 
-    //print whole userList to client
+    /**
+     * when called the Method prints out the whole list of the hashmap containing the Users' name and
+     * their sockets in pairs
+     */
+
     public void printClientList(){
         for (User u: clientList.keySet() ) {
             System.out.println(u.getName());
