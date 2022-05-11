af85ccd62e028044ad41cefd308e4fd4cac2799e
engint <Tolga.Engin@campus.lmu.de>
2022-05-11 17:49:14 +0200

    LoginRequest was implemented

--- love_letter/src/main/java/bb/love_letter/networking/LoginRequest.java
+++ love_letter/src/main/java/bb/love_letter/networking/LoginRequest.java
@@ -1,8 +1,24 @@
 package bb.love_letter.networking;
 
+import bb.love_letter.game.User;
+
 /**
  *
  * @author Tolga Engin
  */
 public class LoginRequest {
+
+    public User user;
+
+    public LoginRequest(User){
+        this.user=user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User newUser) {
+        this.user = newUser;
+    }
 }
