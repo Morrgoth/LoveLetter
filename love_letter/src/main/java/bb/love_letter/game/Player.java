e081a5773863fef24f535d023632fd450a57d22c
Muqiu Wang <wangm@cip.ifi.lmu.de>
2022-05-12 14:13:20 +0200

    attributes for Players added

--- love_letter/src/main/java/bb/love_letter/game/Player.java
+++ love_letter/src/main/java/bb/love_letter/game/Player.java
@@ -1,16 +1,51 @@
 package bb.love_letter.game;
 
+import bb.love_letter.game.characters.Cards;
+
+import java.util.ArrayList;
+
 public class Player extends User{
 
-    private int token;
+    private Cards card1;
+    private Cards card2;
+    private int token = 0;
+    private ArrayList<Cards> discarded = new ArrayList<>();
 
     public Player(String name, int token) {
         super(name);
         this.token = token;
     }
 
+    public Cards getCard1(){
+        return card1;
+    }
+
+    public void setCard1(Cards card1){
+        this.card1 = card1;
+    }
+
+    public void setCard2(Cards card2){
+        this.card2 = card2;
+    }
+
+    public Cards getCard2(){
+        return card2;
+    }
+
     public int getToken(){
         return token;
+    }
+
+    public void setToken(){
+        token++;
+    }
+
+    public int discardedPoints(ArrayList<Cards> discarded){
+        int sum = 0;
+        for(Cards card: discarded){
+            sum += card.getCardPoints();
+        }
+        return sum;
     }
 
 }
