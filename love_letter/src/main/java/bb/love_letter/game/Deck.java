35faae94ea74d39dbbd6e014615c498ddc0963a9
Muqiu Wang <wangm@cip.ifi.lmu.de>
2022-05-12 12:59:03 +0200

    Deck class corrected

--- love_letter/src/main/java/bb/love_letter/game/Deck.java
+++ love_letter/src/main/java/bb/love_letter/game/Deck.java
@@ -1,35 +1,40 @@
 package bb.love_letter.game;
 
 import bb.love_letter.game.characters.*;
-
 import java.util.ArrayList;
+import java.util.Collections;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Deck{
+    ArrayList<Cards> deck = new ArrayList<>();
 
-public class Deck {
-    Guard guard1 = new Guard();
-   // Guard guard2 = new Guard();
-   // Guard guard3 = new Guard();
-   // Guard guard4 = new Guard();
-    Guard guard5 = new Guard();
-    Priest priest1 = new Priest();
-    Priest priest2 = new Priest();
-    Baron baron1 = new Baron();
-    Baron baron2 = new Baron();
-    Handmaid handmaid1 = new Handmaid();
-    Handmaid handmaid2 = new Handmaid();
-    Prince prince = new Prince();
-    King king = new King();
-    Countess countess = new Countess();
-    Princess princess = new Princess();
+    public void initializeDeck(){
+        for(int i = 0; i<5; i++){
+            deck.add(new Guard());
+        }
 
-    ArrayList deck;
+        for(int i = 0; i<2; i++){
+            deck.add(new Priest());
+        }
 
-    {
-        deck = new ArrayList<Cards>(guard1, guard2, guard3, guard4, guard5, priest1, priest2, baron1, baron2, handmaid1, handmaid2, prince, king, countess, princess);
+        for(int i = 0; i<2; i++){
+            deck.add(new Baron());
+        }
+
+        for(int i = 0; i<2; i++){
+            deck.add(new Handmaid());
+        }
+
+        for(int i = 0; i<2; i++){
+            deck.add(new Prince());
+        }
+
+        deck.add(new Princess());
+        deck.add(new King());
+        deck.add(new Countess());
     }
 
-    //shuffle deck before play / pbly in GAME class/interface
+    public void shuffleDeck(){
+        Collections.shuffle(deck);
+    }
 
 }
