f84039c39cb0f5921811b73dc8b436d82334252c
bemantec <amentbence@gmail.com>
2022-05-20 15:11:03 +0200

    Added module export

--- love_letter/src/main/java/module-info.java
+++ love_letter/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module bb.love_letter {
 
 
     exports bb.love_letter.user_interface;
+    exports bb.love_letter.game.characters;
     opens bb.love_letter.user_interface to javafx.fxml;
     exports bb.love_letter.game;
     opens bb.love_letter.game to javafx.fxml;
